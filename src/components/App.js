db8765b9a9a44394a7ceb90047684a5f5f6e2348
emily baier <emilyrbaier@gmail.com>
2019-05-20 11:39:17 -0700

    everything renders, tests pass, looking super amazing

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Red from './Red';
+import Blue from './Blue';
+import Yellow from './Yellow';
+import Header from './Header';
+
+export default function App() {
+  return (
+    <Router>
+      <Header/>
+      <Switch>
+        <Route path="/red" component={Red}/>
+        <Route path="/blue" component={Blue}/>
+        <Route path="/yellow" component={Yellow}/>
+      </Switch>
+    </Router>
+  );
+}
